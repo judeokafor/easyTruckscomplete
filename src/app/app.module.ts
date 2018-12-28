8ff106244c1a3a8bf7119f48ea28ca59f2497c0b
Okafor Jude <okaforjudechukwuebuka@gmail.com>
2018-12-28 06:44:29 +0100

    successfully created a git repo, created basic routes. set up materialize-css, angular2-materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IndexpageComponent } from './components/indexpage/indexpage.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    IndexpageComponent,
+    LoginComponent,
+    RegisterComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
